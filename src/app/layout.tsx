b393188dcc6d0fd83b19a984e9f3f17310f22c5d
Charles Crockett <charlescrockett.44@gmail.com>
2025-06-27 15:23:30 -0500

    feat: add Microsoft Clarity heatmap and session recording script to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,16 @@ export default function RootLayout({
             console.log('🔧 GA4 Script loaded and configured for G-PTPLFZNX4D');
           `}
         </Script>
+        {/* Microsoft Clarity */}
+        <script type="text/javascript" dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "s67j4vqdgw");
+          `
+        }} />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
