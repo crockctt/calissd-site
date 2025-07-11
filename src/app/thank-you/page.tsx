c0c1b5ca802efa25606e478895dd75383b75509b
Charles Crockett <charlescrockett.44@gmail.com>
2025-07-11 15:50:29 -0500

    fix: use exact GTM script with HTML comments in every page file

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -6,19 +6,19 @@ export default function ThankYou() {
   return (
     <>
       <Head>
-        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
+              <!-- Google Tag Manager -->
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-KFH4QS6J');
+              <!-- End Google Tag Manager -->
             `,
           }}
         />
-        {/* End Google Tag Manager */}
       </Head>
       {/* Google Tag Manager (noscript) */}
       <noscript>
